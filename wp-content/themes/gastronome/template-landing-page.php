2eed0682a02463225cf1f3fff23617eb8378fce9
LermeRomain <romainlerme@hotmail.fr>
2020-11-30 23:06:41 +0100

    cc

--- wp-content/themes/gastronome/template-landing-page.php
+++ wp-content/themes/gastronome/template-landing-page.php
@@ -8,8 +8,39 @@
                 A tous les amoureux de la bonne bouffe, aux créatifs, aux traditionnels, à ceux qui reproduisent
                 l'original et à ceux qui le sont, vous êtes la pièce maîtresse qui constitue la grande cuisine.
             </p>
-            <a href="#" class="hero-scroller">Découvrir</a>
+            <a href="#" class="hero-scroller"><img src="http://localhost/gastronome-theme/wp-content/uploads/2020/11/Vector-144.png">Découvrir</a>
         </div>
     </div>
+    <div class="container">
+        <div class="section">
+            <p class="section-txt">
+                Vous pensez que vous n’arriverez jamais à faire monter des blancs en neige ou que la gastronomie n’est
+                pratiquée que par les chefs culainaires ?
+
+                Détrompez-vous. La gastronomie est accesible à tous.
+                Il ne vous manque qu’un coup de pouce.
+            </p>
+        </div>
+    </div>
+
+        <div class="illustration">
+            <div class="illustration-img">
+                <img class="img-fluid" src="http://localhost/gastronome-theme/wp-content/uploads/2020/11/image1.png" align="center">
+            </div>
+        </div>
+
+    <div class="container">
+        <div class="voyage">
+            <p class="voyage-txt">
+                <span>Vous allez voyager !</span> Redécouvrez les succulantes recettes de grand-mère ou les petites innovations
+                culinaire à vous en mettre plein les papilles.
+            </p>
+            <p class="voyage-box">
+                Voir toutes les recettes.
+            </p>
+
+        </div>
+    </div>
+
 
 <?php get_footer(); ?>
