ff1d49a4eb64bd2ac94ff44558209aae36ef9908
LermeRomain <romainlerme@hotmail.fr>
2020-12-01 16:51:01 +0100

    hop hop

--- wp-content/themes/gastronome/template-landing-page.php
+++ wp-content/themes/gastronome/template-landing-page.php
@@ -16,7 +16,8 @@
             <p class="section-txt">
                 Vous pensez que vous n’arriverez jamais à faire monter des blancs en neige ou que la gastronomie n’est
                 pratiquée que par les chefs culainaires ?
-
+                <br>
+                <br>
                 Détrompez-vous. La gastronomie est accesible à tous.
                 Il ne vous manque qu’un coup de pouce.
             </p>
@@ -36,9 +37,105 @@
                 culinaire à vous en mettre plein les papilles.
             </p>
             <p class="voyage-box">
-                Voir toutes les recettes.
+                Voir toutes les recettes
             </p>
 
+        </div>
+    </div>
+    <div class="container">
+        <div class="recette">
+            <p class="recette-txt">
+                <span1>Recettes.</span1>  Conçues par de grands chefs mais aussi par des amateurs,
+                ces recettes vont vous aider à faire bien et beau, simplement, avec des produits accessibles de qualité.
+            </p>
+        </div>
+    </div>
+    <div class="illustration">
+        <div class="illustration-img">
+            <img class="img-fluid" src="http://localhost/gastronome-theme/wp-content/uploads/2020/12/chef-preparing-food-in-the-kitchen-chef-cooking-1.png" align="center">
+        </div>
+    </div>
+    <div class="container">
+        <div class="voyage">
+            <p class="voyage-txt">
+                <span3>Vous doutiez de votre talent ?</span3> Nous avons tous déjà cuisiner de façon
+                gastronomique. Ou du moins essayer. C’est le moment d’en prendre conscience.
+            </p>
+            <p class="voyage-box1">
+                Voir tous le témoignages
+            </p>
+        </div>
+    </div>
+    <div class="container">
+        <div class="recette">
+            <p class="recette-txt">
+                <span1>Témoignages.</span1>  Découvrez les fabuleuses histoires d’Hanaé, de Théophile, de
+                Julien, de Constance et de bien d’autres qui ont trouvés leur passion dans la cuisine.
+            </p>
+        </div>
+    </div>
+    <div class="illustration">
+        <div class="illustration-img">
+            <img class="img-fluid" src="http://localhost/gastronome-theme/wp-content/uploads/2020/12/Avis-e1606832178195.png" >
+        </div>
+    </div>
+    <div class="d-inline-block">
+        <div class="temoignages">
+                <p class="temoignages-txt">
+                    <span>Hanaé</span><br><br>
+                    J’ai toujours été passionné de cuisine mais je n’osais pas montrer mes plats car je ne les trouvais pas
+                    à la hauteur. Gastrono’me est là pour nous prouver le contraire.
+                </p>
+            <p class="temoignages-txt2">
+                <span>Titouan</span><br><br>
+                J’ai toujours eu un manque de motivation vis à vis de la cuisine car je la trouvais compliquée.
+                Mais grace à Gastrono’me je me suis rendu compte que c’est finalement accessible.
+            </p>
+        </div>
+    </div>
+    <div class="d-inline-block">
+        <div class="temoignages">
+            <p class="temoignages-txt">
+                <span>Julien</span><br><br>
+                J’adore cuisiner pour mes amis, je suis le cuistot de la bande.
+                Le projet Gastrono’me est génial, j’apprends de nombreuses choses et me perfectionne.
+            </p>
+            <p class="temoignages-txt2">
+                <span>Constance</span><br><br>
+                La gastronomie me semblait assez fermée et peu accessible.
+                Maintenant, avec Gastrono'me, je peux enfin découvrir la cuisine et épater ma famille.
+            </p>
+        </div>
+    </div>
+    <br>
+    <br>
+    <br>
+    <br>
+    <div class="illustration">
+        <div class="illustration-img">
+            <img class="img-fluid" src="http://localhost/gastronome-theme/wp-content/uploads/2020/12/meat-tomato-sauce-potato-greens-cucubmer-side-view-1.png" align="center">
+        </div>
+    </div>
+    <div class="container">
+        <div class="voyage">
+            <p class="voyage-txt">
+                <span>Articles.</span>  Des papiers qui parlent concretement de
+                notre cuisine, son patrimoine et de cet art qu’est la gastronomie. Pour tous.
+            </p>
+            <p class="voyage-box1">
+                Voir tous le témoignages
+            </p>
+        </div>
+    </div>
+    <div class="container">
+        <div class="explore">
+            <p class="explore-txt">
+                <span>On touche avec les yeux !</span>   Explorez le merveilleux monde de la gastronomie au
+                travers de posts, stories sur nos médias sociaux. Retrouvez-nous sur Instagram, Facebook, Twitter et Pinterest.
+            </p>
+            <br>
+            <br>
+            <a href="#" class="hero-scroller"><img src="http://localhost/gastronome-theme/wp-content/uploads/2020/12/Coeur.png"> Nous suivres</a>
         </div>
     </div>
 
